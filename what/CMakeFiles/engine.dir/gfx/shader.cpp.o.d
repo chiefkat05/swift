6bd3a8e5a164f49f501ca50eec7080fd89e6c51f
chiefkat05 <jamesville05@gmail.com>
2024-05-06 19:19:37 -0500

    what

--- what/CMakeFiles/engine.dir/gfx/shader.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/engine.dir/gfx/shader.cpp.o: \
- /home/chiefkat/Documents/vscodeprojects/swift/gfx/shader.cpp \
- /usr/include/stdc-predef.h \
- /home/chiefkat/Documents/vscodeprojects/swift/gfx/shader.h
