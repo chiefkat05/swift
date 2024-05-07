ca6815448a5ac6fb4e7388b0c0c620f481465dfc
chiefkat05 <jamesville05@gmail.com>
2024-05-06 19:23:11 -0500

    window open :)

---
+++ build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/gfx/shader.cpp.o"
+  "CMakeFiles/engine.dir/gfx/shader.cpp.o.d"
+  "CMakeFiles/engine.dir/main.cpp.o"
+  "CMakeFiles/engine.dir/main.cpp.o.d"
+  "CMakeFiles/engine.dir/nms/glad/src/glad.c.o"
+  "CMakeFiles/engine.dir/nms/glad/src/glad.c.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
