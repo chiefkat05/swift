0a298e5fac3135a1fbc2c035545e9a372ecee45a
chiefkat05 <jamesville05@gmail.com>
2024-05-06 19:19:26 -0500

    date

--- graphics.h
+++ graphics.h
@@ -12,11 +12,41 @@ struct sprite
 {
 };
 
+void framebuffer_size_callback(GLFWwindow *win, int width, int height)
+{
+    glViewport(50, 50, width - 100, height - 100);
+}
+
 // when British person sees images on the screen
 void graphics_init()
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "swift", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "glfw window failed to open\n";
+        glfwTerminate();
+        return;
+    }
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+    // gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    // glViewport(50, 50, 1180, 620);
+    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
+
+    glfwTerminate();
 }
--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ int main()
     std::string input;
     std::cout << "Welcome to swift!\n";
 
-    while (getline(std::cin, input) && input != "quit")
-    {
-        std::cout << "I hear you say " << input << " but it doesn't do anything yet.\n";
-    }
+    graphics_init();
+    // while (getline(std::cin, input) && input != "quit")
+    // {
+    //     std::cout << "I hear you say " << input << " but it doesn't do anything yet. Try 'start' to open a window with the game!\n";
+    // }
 
-    std::cout << "Game will close.\n";
+    std::cout << "Game will close.\n"; // switch to x11 omigosh
     return 0;
 }
